df82098fe11c8ce944b7129a034c700f1ccc8e34
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2026-04-12 15:47:33 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void solve(int pos, vector<int>& temp, vector<int>& nums, vector<vector<int>>& ans) {
+        ans.push_back(temp);
+        for(int i = pos; i < nums.size(); i++) {
+            if(i > pos && nums[i] == nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            solve(i+1, temp, nums, ans);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        solve(0, temp, nums, ans);
+        return ans;
+    }
+};
